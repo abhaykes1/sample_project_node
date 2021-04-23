1e01ce54acabc906b4c587c5d983df840859dd76
abhayKesharwani <abhay.kesharwani@hackerearth.com>
2021-04-23 13:03:26 +0530

    added cors

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
-
-//Allow all requests from all domains & localhost
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
-  next();
-});
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({
