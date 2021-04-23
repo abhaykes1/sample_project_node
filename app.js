e8553585bcb630dfcc39d24ac718e1d5ab243a12
abhayKesharwani <abhay.kesharwani@hackerearth.com>
2021-04-23 13:28:00 +0530

    removed cors

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const cors = require('cors');
 
 const app = express();
-app.use(cors())
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use(express.urlencoded({
 extended: true
